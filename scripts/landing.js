82a8131f66f00bf08b5a5af2bde16495f004550b
Vlad Ryabukha <vladryabukha7@gmail.com>
2017-06-20 18:17:07 -0500

    Updated checkpoint 10

--- scripts/landing.js
+++ scripts/landing.js
@@ -18,9 +18,9 @@ var animatePoints = function (points) {
 window.onload = function () {
   // Automatically animate the points on a tall screen where scrolling can't trigger the animation
   if (window.innerHeight > 950) {
-      animatePoints(pointsArray);
+    animatePoints(pointsArray);
   }
-  
+
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
